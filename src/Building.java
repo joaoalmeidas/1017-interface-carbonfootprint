7989f2d8cc1f6916f63426e677664ea74bf54a31
joao <joao182cx@gmail.com>
2019-01-14 13:16:21 -0200

    Criação dos construtores.

--- src/Building.java
+++ src/Building.java
@@ -4,4 +4,25 @@ public class Building {
 	private double area;
 	private double height;
 	
+	
+	public Building(double area, double height) {
+		
+		if(area <= 0) {
+			
+			throw new IllegalArgumentException("Área deve ser > 0");
+			
+		}
+		
+		if(height <= 0) {
+			
+			throw new IllegalArgumentException("Altura deve ser > 0");
+			
+		}
+		
+		this.area = area;
+		this.height = height;
+	}
+	
+	
+	
 }
--- src/Car.java
+++ src/Car.java
@@ -4,4 +4,25 @@ public class Car {
 	private int numberOfSeats;
 	private int horsepower;
 	
+	
+	public Car(int numberOfSeats, int horsepower) {
+		
+		if(numberOfSeats <= 0) {
+			
+			throw new IllegalArgumentException("Numero de assentos deve ser > 0");
+			
+		}
+		
+		if(numberOfSeats <= 0) {
+			
+			throw new IllegalArgumentException("Horsepower deve ser > 0");
+			
+		}
+		
+		this.numberOfSeats = numberOfSeats;
+		this.horsepower = horsepower;
+	}
+	
+	
+	
 }
